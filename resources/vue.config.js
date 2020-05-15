fa65b5af23e3f78af220cc96342f67e7b0b744fc
DandiIndraWijaya <dandiindra29@gmail.com>
2020-05-15 14:28:52 +0700

    Revert "Revert "commit pertama""
    This reverts commit c55dd1f8904cdd480464ce6532d453e9cebd72b1.

---
+++ resources/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    outputDir: './dist',
+    indexPath: '../index.html',
+    publicPath: process.env.NODE_ENV == 'production' ? 'dist/' : './',
+    devServer: {
+        public: 'localhost:8080',
+        host: 'localhost',
+        port: '8080'
+    }
+}
