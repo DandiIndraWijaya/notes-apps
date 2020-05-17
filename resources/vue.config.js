155111c5db98d69cbef5cd264294289549c2aff8
DandiIndraWijaya <dandiindra29@gmail.com>
2020-05-17 10:44:14 +0700

    Bikin gitignore agar tidak tracking hasil deploy dan atur ulang vue.config.js

--- resources/vue.config.js
+++ resources/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    outputDir: './dist',
+    outputDir: '../dist',
     indexPath: '../index.html',
     publicPath: process.env.NODE_ENV == 'production' ? 'dist/' : './',
     devServer: {
